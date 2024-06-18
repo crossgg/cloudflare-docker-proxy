cdcd994dca4bfdfeddc668bd062dada06262c346
crosscc <37495419+crossgg@users.noreply.github.com>
2024-06-18 17:49:02 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import DOCS from './help.html'
+
 addEventListener("fetch", (event) => {
   event.passThroughOnException();
   event.respondWith(handleRequest(event.request));
@@ -7,16 +9,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.haoz.us.kg": dockerHub,
+  "quay.haoz.us.kg": "https://quay.io",
+  "gcr.haoz.us.kg": "https://gcr.io",
+  "k8s-gcr.haoz.us.kg": "https://k8s.gcr.io",
+  "k8s.haoz.us.kg": "https://registry.k8s.io",
+  "ghcr.haoz.us.kg": "https://ghcr.io",
+  "cloudsmith.haoz.us.kg": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.haoz.us.kg": dockerHub,
 };
 
 function routeByHosts(host) {
@@ -41,6 +43,15 @@ async function handleRequest(request) {
         status: 404,
       }
     );
+  }
+  // return docs
+  if (url.pathname === "/") {
+    return new Response(DOCS, {
+      status: 200,
+      headers: {
+        "content-type": "text/html"
+      }
+    });
   }
   const isDockerHub = upstream == dockerHub;
   const authorization = request.headers.get("Authorization");
